13c88b7185d4f47bab00be8417b81145829ac5ea
Babiker Babiker <Bbabiker@asu.edu>
2026-05-15 20:24:40 -0700

    Initial Track-Mate setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Home from './component/homepage.vue'
+
+export default {
+  components: {
+    Home
+  }
+}
+</script>
+
+<template>
+  <h1>Truck-Mate App</h1>
+
+  <Home/>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/component/homepage.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'home-page',
+
+  data() {
+    return {
+      email:"",
+      password:"",
+
+    }
+  },
+
+  methods:{
+    credential(){
+        alert(`Email: ${this.email} / Password: ${this.password}`);
+
+    }
+
+  }
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <label style="padding: 50px"> Enter email</label> <input v-model="email" type="text" placeholder="Enter email" />
+  <br />
+  <label style="padding: 50px"> Enter email</label>
+  <input v-model="password" type="password" placeholder="Enter password" /> <br />
+  <button style="margin-left: 200px" @click="credential">
+    Login
+  </button>
+</template>
+
+<style scoped></style>
